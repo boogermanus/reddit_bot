0023aa3f1cc775c42c9e3f4673f7e5ae2935ca35
Jordan Woodruff <jordan@neko.local>
2017-05-13 14:03:56 -0500

    Added Reddit Bot

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
 var snoowrap = require('snoowrap');
 var EventEmitter = require('events');
 var key = require('./redditkey');
+var bot = require('./redditbot.js');
 
 var r = new snoowrap(key.RedditAPIKey);
 
-var usersToFollow = ["boogermanus"];
-var previousItems = []
-var emitter = new EventEmitter();
+var redditBot = new bot.RedditBot(r);
 
-var getDisplay = function(data) {
-    return data.subreddit_name_prefixed + " -> " + data.title;
-}
+redditBot.emitter.on('add', console.log);
 
-var reportRecents = function(data) {
-    if(previousItems.length == 0) {
-        for(upvoted of data) {
-            var display  = getDisplay(upvoted);
-            previousItems.push(display);
-            emitter.emit('add', display);
-        }
-    }
-    else {
-        for(upvoted of data) {
-
-            var display = getDisplay(upvoted);
-
-            if(previousItems.findIndex(s => s === display) === -1)
-            {
-                //remove last item in list and add this item to top of stack
-                previousItems.splice(-1,1);
-                previousItems.splice(0,0,display);
-
-                emitter.emit('add', display);
-            }
-        }
-    }
-}
-
-var followUserUpvotes = function() {
-    for(user of usersToFollow) {
-        r.getUser(user).getUpvotedContent({'limit': 10 })
-        .then(reportRecents)
-    }
-}
-
-setInterval(followUserUpvotes, 15000);
-emitter.on('add',console.log);
+setInterval(function() {
+    redditBot.followUpvotes("boogermanus");
+}, 10000);
---
+++ redditbot.js
@@ -0,0 +1,45 @@
+var EventEmitter = require('events').EventEmitter;
+
+var getDisplay = function(data) {
+    return data.subreddit_name_prefixed + " -> " + data.title;
+}
+
+var RedditBot = function(wrap) {
+    this.snooWrap = wrap;
+    this.previousUpvotes = [];
+    this.emitter = new EventEmitter();
+};
+
+RedditBot.prototype.followUpvotes = function(user) {
+    this.snooWrap.getUser(user).getUpvotedContent({'limit': 10 })
+        .then((data) => this.reportRecents(data))
+}
+
+RedditBot.prototype.reportRecents = function(data) {
+    if(this.previousUpvotes.length == 0) {
+        for(upvoted of data) {
+            var display  = getDisplay(upvoted);
+            this.previousUpvotes.push(display);
+            this.emitter.emit('add', display);
+        }
+    }
+    else {
+        for(upvoted of data) {
+
+            var display = getDisplay(upvoted);
+
+            if(this.previousUpvotes.findIndex(s => s === display) === -1)
+            {
+                //remove last item in list and add this item to top of stack
+                this.previousUpvotes.splice(-1,1);
+                this.previousUpvotes.splice(0,0,display);
+
+                this.emitter.emit('add', display);
+            }
+        }
+    }
+}
+
+module.exports = {
+    RedditBot: RedditBot
+};
