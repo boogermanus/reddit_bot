e36afc0eee0ae51bc6a958f6c0a715cd3930571f
Jordan Woodruff <jordan@neko.local>
2017-08-29 18:19:40 -0500

    Ditch code

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 var snoowrap = require('snoowrap');
-var EventEmitter = require('events');
 var key = require('./redditkey');
-var bot = require('./redditbot.js');
 
 var r = new snoowrap(key.RedditAPIKey);
 
-var redditBot = new bot.RedditBot(r);
-
-redditBot.addUser("boogermanus");
-redditBot.run();
 
