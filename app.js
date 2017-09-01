47cd3c6cfbd2a77cd393d5e3d7c05dbdfa3a2466
Woodruff <Jordan.Woodruff@tylertech.com>
2017-09-01 16:44:43 -0500

    Basic Processing Loop

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ let r = new snoowrap(key.RedditAPIKey);
 let redditBot = new bot.RedditBot(r);
 redditBot.run();
 
-redditBot.on('data',(json) => {
-    console.log('Event Count: ' + json.count);
+redditBot.on('data',(data) => {
+    let date = new Date();
+    console.log("Data Update -> " + date.toString())
+    for(let sub of data) {
+        console.log(sub.title);
+    }
 });
 
--- bot.js
+++ bot.js
@@ -4,13 +4,47 @@ class RedditBot extends EventEmitter {
     constructor(snoo) {
         super();
         this.snoo = snoo;
-        this.count = 1;
+        this.recentSubmissions = [];
     }
 
     run() {
-        setInterval(() => {
-            this.emit('data',{count:this.count++})
-        }, 1000);
+        setInterval(() => {this.refresh()}, 10000);
+    }
+    
+    async getSubreddit(subredditName) {
+        let subreddit = this.snoo.getSubreddit(subredditName);
+        let data = await subreddit.getNew({limit: 10});
+        return data;
+    }
+
+    async refresh() {
+        let data = await this.getSubreddit('pics');
+
+        //if not data, push everything
+        if(this.recentSubmissions.length === 0) {
+            for(let submission of data) {
+                this.recentSubmissions.push({
+                    title:submission.title
+                });
+            }
+
+            this.emit('data', this.recentSubmissions);
+        }
+        else {
+            //we have data, so we need to process the incoming data to determine
+            //if it is new.
+            let newData = [];
+            for(let submission of data) {
+                if(this.recentSubmissions.findIndex(i => i.title === submission.title) === -1) {
+                    let newSub = {title:submission.title};
+                    this.recentSubmissions.pop();
+                    this.recentSubmissions.unshift(newSub);
+                    newData.push(newSub);
+                }
+            }
+
+            this.emit('data', newData);
+        }
     }
 }
 
